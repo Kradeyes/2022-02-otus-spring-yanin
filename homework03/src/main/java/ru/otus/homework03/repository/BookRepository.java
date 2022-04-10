3aca8d93b3b68c3bf4b99a2936fb388ecd20e8de
Kradeyes <yanin.r.a@mail.ru>
2022-04-10 19:05:30 +0300

    3.1 n+1 fetchType

--- homework03/src/main/java/ru/otus/homework03/repository/BookRepository.java
+++ homework03/src/main/java/ru/otus/homework03/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package ru.otus.homework03.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.homework03.domain.Book;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -14,4 +16,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findBookByAuthor_Id(long bookAuthorId);
 
     Optional<Book> findBookByGenre_Id(long bookGenreId);
+
+    @EntityGraph(attributePaths = {"author", "genre"})
+    @Override
+    List<Book> findAll();
 }
