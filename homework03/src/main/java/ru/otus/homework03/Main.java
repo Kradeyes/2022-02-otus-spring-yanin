87f966653a55e826924fcc11ce194c6abb5f9c1a
Kradeyes <yanin.r.a@mail.ru>
2022-06-01 12:45:39 +0300

    4.7 task

--- homework03/src/main/java/ru/otus/homework03/Main.java
+++ homework03/src/main/java/ru/otus/homework03/Main.java
@@ -2,17 +2,18 @@ package ru.otus.homework03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.ApplicationContext;
 import ru.otus.homework03.service.GetGenreRestService;
 
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-//        ApplicationContext ctx = SpringApplication.run(Main.class, args);
-//        GetGenreRestService service = ctx.getBean(GetGenreRestService.class);
-//        System.out.println(service.getGenre(1));
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        GetGenreRestService service = ctx.getBean(GetGenreRestService.class);
+        System.out.println(service.getGenre(1));
     }
 }
--- homework03/src/main/java/ru/otus/homework03/service/GetGenreRestServiceImpl.java
+++ homework03/src/main/java/ru/otus/homework03/service/GetGenreRestServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.otus.homework03.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 import ru.otus.homework03.dto.GenreDto;
 
+import java.util.Arrays;
 
 @Service
 public class GetGenreRestServiceImpl implements GetGenreRestService {
@@ -14,9 +17,23 @@ public class GetGenreRestServiceImpl implements GetGenreRestService {
 
     private RestOperations rest = new RestTemplate();
 
+    @HystrixCommand(commandProperties ={
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds" , value = "3000")} ,
+            fallbackMethod = "defaultGenreFromGenreService")
     @Override
     public GenreDto getGenre(long id) {
         log.info("Request");
+        try {
+            System.out.println("Сплю 5 секунд");
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
         return rest.getForObject("http://localhost:8090/api/v1/genre/" + id, GenreDto.class);
+    }
+
+    private GenreDto defaultGenreFromGenreService(long id) {
+        log.info("The service is currently unavailable, the default genre is set. Please try to get the current genre later");
+        return new GenreDto(id, "SomeDefaultGenre");
     }
 }
